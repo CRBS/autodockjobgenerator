3e1478626d3f67c5d55963b3cb98e18fec030ca8
Christopher Churas <churas@gmail.com>
2013-08-13 17:01:19 -0700

    improved documentation and now pass in CompressDirectory object via constructor

--- src/main/java/edu/ucsd/crbs/autodockwrapper/job/CompressInputDirTask.java
+++ src/main/java/edu/ucsd/crbs/autodockwrapper/job/CompressInputDirTask.java
@@ -8,12 +8,15 @@ import edu.ucsd.crbs.autodockwrapper.io.CompressDirectory;
 import edu.ucsd.crbs.autodockwrapper.io.CompressDirectoryImpl;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * Compresses paths passed in via constructor using the {@link Runnable} interface
+ * allowing jobs to be run in background threads.
+ * 
  * @author churas
  */
 public class CompressInputDirTask implements Runnable {
@@ -22,18 +25,29 @@ public class CompressInputDirTask implements Runnable {
     
     private int _taskId;
     private String _inputsDir;
-    
-    public CompressInputDirTask(int taskId,final String inputsDir){
+    private CompressDirectory _cd;
+
+    /**
+     * Constructor
+     * @param cd CompressDirectory object that does the compression
+     * @param taskId id of task to compress
+     * @param inputsDir Inputs folder where task to comrpess resides
+     */
+    public CompressInputDirTask(CompressDirectory cd,int taskId,final String inputsDir){
         _taskId = taskId;
         _inputsDir = inputsDir;
+        _cd = cd;
     }
     
+    /**
+     * Compresses path set in constructor using CompressDirectory object also
+     * set in the constructor.  
+     */
     @Override
     public void run() {
         try {
             logger.debug("Compressing directory for task {}",_taskId);
-            CompressDirectory cd = new CompressDirectoryImpl();
-            cd.compressDirectory(_taskId, _inputsDir);
+            _cd.compressDirectory(_taskId, _inputsDir);
            
             logger.debug("Deleting directory for task {}",_taskId);
 
