e160c397e3dfc832bded8f449fdec75a6780ac6b
Christopher Churas <churas@gmail.com>
2013-08-13 17:03:34 -0700

    code now creates the executor service and manages the threads so progress can be reported to the user

--- src/main/java/edu/ucsd/crbs/autodockwrapper/App.java
+++ src/main/java/edu/ucsd/crbs/autodockwrapper/App.java
@@ -8,16 +8,31 @@ import edu.ucsd.crbs.autodockwrapper.io.*;
 import edu.ucsd.crbs.autodockwrapper.job.JobGenerator;
 import edu.ucsd.crbs.autodockwrapper.job.JobGeneratorImpl;
 import java.io.FileReader;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import org.apache.commons.io.IOUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * PROTOTYPE CODE!!!!!!!!!!!!!!
  * This is the main entry class for AutoDockWrapper. AutoDockWrapper is a
- * program that generates Auto Dock Vina jobs and optionally will run them on
- * HPC compute resources via Panfish.
+ * program that generates Auto Dock Vina jobs suitable for running via Panfish
+ * on HPC resources.<P/>
+ * 
+ * This program invokes helper classes to do four main tasks.  The links
+ * below describe these tasks.<P/>
+ * 
+ * {@link edu.ucsd.crbs.autodockwrapper.io.JobDirCreatorImpl}<BR/>
+ * {@link edu.ucsd.crbs.autodockwrapper.io.AutoDockScriptCreatorImpl}<BR/>
+ * {@link edu.ucsd.crbs.autodockwrapper.io.PanfishAutoDockScriptCreatorImpl}<BR/>
+ * {@link edu.ucsd.crbs.autodockwrapper.job.JobGeneratorImpl}<BR/>
+ * 
  *
  * @author Christopher Churas
  */
@@ -25,15 +40,25 @@ public class App {
 
     final static Logger logger = LoggerFactory.getLogger(App.class);
     
-    public static final String LIGANDS_ARG = "ligands";
-    public static final String RECEPTORS_ARG = "receptors";
-    public static final String OUTPUT_ARG = "outputjobdir";
-    public static final String HELP_ARG = "h";
+    static final String LIGANDS_ARG = "ligands";
+    static final String RECEPTORS_ARG = "receptors";
+    static final String OUTPUT_ARG = "outputjobdir";
+    static final String HELP_ARG = "h";
 
+    /**
+     * The default arguments passed to Auto Dock jobs: {@value}
+     */
     public static final String DEFAULT_ARGS = "--center_x 41.1100 --center_y 34.9382 --center_z 35.8160 --size_x 25.0000 --size_y 25.0000 --size_z 25.0000 --cpu 2";
+    
+    /**
+     * The default path to Auto Dock vina binary: {@value}
+     */
     public static final String DEFAULT_VINA_BIN = "$PANFISH_BASEDIR/home/churas/bin/autodock_vina_1_1_2/bin/vina";
     
-    
+
+    /**
+     * Command line application that generates Auto Dock Vina jobs
+     */
     public static void main(String[] args) {
         try {
 
@@ -77,11 +102,37 @@ public class App {
             AutoDockScriptCreator adsc = getAutoDockScriptCreator();
             adsc.createAutoDockScript(outputJobDir, DEFAULT_ARGS,DEFAULT_VINA_BIN);
             
+              //write out the panfish auto dock script file into outputjobdir
+            PanfishAutoDockScriptCreator padc = getPanfishAutoDockScriptCreator();
+            padc.createPanfishAutoDockScript(outputJobDir);
+            
+            
+            ExecutorService es = getExecutorService();
+            List<Future> taskList = Collections.synchronizedList(new LinkedList<Future>());
             //generate jobs
-            JobGenerator jg = getJobGenerator();
-            jg.createJobs(outputJobDir, IOUtils.readLines(new FileReader(ligandFile)), 
+            JobGenerator jg = getJobGenerator(es,taskList,outputJobDir, 
+                    IOUtils.readLines(new FileReader(ligandFile)), 
                     IOUtils.readLines(new FileReader(receptorFile)));
             
+            long startTime = System.currentTimeMillis();
+            taskList.add(es.submit(jg));
+            
+            
+            threadSleep(20000);
+            long totalJobs = jg.getTotalJobs();
+            long percentComplete;
+            
+            //okay we submitted the jobs now we can let the user know the status
+            //by looking at the count of tasks remaining
+            int removeCount = 0;
+            while(taskList.size() > 0){
+               percentComplete = getPercentComplete(removeCount,totalJobs);
+               System.out.println(percentComplete+"% ("+removeCount+" of "+totalJobs+") job(s) created.  "+estimatedTimeRemaining(removeCount,totalJobs,startTime));
+               threadSleep(20000);
+               removeCount += removeCompletedTasks(taskList);
+            }
+            
+            es.shutdown();
             
         } catch (Exception ex) {
             logger.error("Caught Exception.  Exiting..", ex);
@@ -89,15 +140,71 @@ public class App {
         }
     }
     
-    public static JobDirCreator getJobDirCreator(){
+    static void threadSleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (Exception ex) {
+        }
+    }
+    
+    static String estimatedTimeRemaining(long numberCompleted,long totalCount,long startTime){
+        long curTime = System.currentTimeMillis();
+        if (numberCompleted <= 0){
+            return "Unknown time remaining.";
+        }
+        return Long.toString(Math.round((((double)(curTime-startTime)*(double)totalCount)/(double)numberCompleted)/1000.0))+" seconds remaining.";
+    }
+    
+    static long getPercentComplete(long numberCompleted, long totalCount){
+        return Math.round(((double)numberCompleted/(double)totalCount)*100.0);
+    }
+    
+    
+    static JobDirCreator getJobDirCreator(){
         return new JobDirCreatorImpl();
     }
     
-    public static AutoDockScriptCreator getAutoDockScriptCreator(){
+    static AutoDockScriptCreator getAutoDockScriptCreator(){
         return new AutoDockScriptCreatorImpl();
     }
     
-    public static JobGenerator getJobGenerator(){
-        return new JobGeneratorImpl();
+    static PanfishAutoDockScriptCreator getPanfishAutoDockScriptCreator(){
+        return new PanfishAutoDockScriptCreatorImpl();
+    }
+    
+    static JobGenerator getJobGenerator(ExecutorService es,List<Future> taskList,final String outputJobDir,List<String> ligands,List<String> receptors){
+        return new JobGeneratorImpl(es,taskList,outputJobDir,ligands,receptors);
+    }
+    
+     /**
+     * This method creates an {@link java.util.concurrent.ExecutorService} with a thread pool size
+     * set to {@link edu.ucsd.crbs.autodockwrapper.Constants#PERCENT_OF_CORES_TO_USE_FOR_COMPRESSION} X
+     * {@link java.lang.Runtime.getRuntime()#availableProcessors() Runtime.getRuntime().availableProcessors()} or 1 if value is less then 1.
+     * @return ExecutorService
+     */
+    static ExecutorService getExecutorService(){
+        //Create a threadpool that is 90% size of number of processors on system
+        //with a minimum size of 1.
+        int threadPoolSize = (int)Math.round((double)Runtime.getRuntime().availableProcessors()*
+                                              Constants.PERCENT_OF_CORES_TO_USE_FOR_COMPRESSION);
+        if (threadPoolSize < 1){
+            threadPoolSize = 1;
+        }
+        logger.debug("Creating threadpool size of {} to compress all the input directories",threadPoolSize);
+        return Executors.newFixedThreadPool(threadPoolSize);
+    }
+    
+    static int removeCompletedTasks(List<Future> taskList){
+        Future f;
+        int removeCount = 0;
+        Iterator<Future> itr = taskList.iterator();
+        while(itr.hasNext()){
+            f = itr.next();
+            if (f.isDone() || f.isCancelled()){
+                removeCount++;
+                itr.remove();
+            }
+        }
+        return removeCount;
     }
 }
