98612b092381e45c9851d4541c008fb078e253b1
Christopher Churas <churas@gmail.com>
2013-08-26 13:40:43 -0700

    improved documentation

--- src/main/java/edu/ucsd/crbs/autodockwrapper/io/PanfishAutoDockScriptCreator.java
+++ src/main/java/edu/ucsd/crbs/autodockwrapper/io/PanfishAutoDockScriptCreator.java
@@ -7,11 +7,16 @@ package edu.ucsd.crbs.autodockwrapper.io;
 import java.io.IOException;
 
 /**
- *
+ * Interface to create Panfish Auto Dock Script.
  * @author churas
  */
 public interface PanfishAutoDockScriptCreator {
     
+    /**
+     * Implementing methods should create {@value edu.ucsd.crbs.autodockwrapper.Constants#PANFISH_AUTO_DOCK_SCRIPT} under <b>outputJobDir</b> directory.
+     * @param outputJobDir Path under which the Panfish Auto Dock script should be created.
+     * @throws IOException 
+     */
     public void createPanfishAutoDockScript(final String outputJobDir) throws IOException;
     
 }
