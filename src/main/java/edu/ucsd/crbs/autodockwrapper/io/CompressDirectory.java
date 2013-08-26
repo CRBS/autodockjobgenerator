e33df16e058d13a06c1286dd558acd5381619ccf
Christopher Churas <churas@gmail.com>
2013-08-26 13:39:59 -0700

    improved documentation

--- src/main/java/edu/ucsd/crbs/autodockwrapper/io/CompressDirectory.java
+++ src/main/java/edu/ucsd/crbs/autodockwrapper/io/CompressDirectory.java
@@ -7,12 +7,18 @@ package edu.ucsd.crbs.autodockwrapper.io;
 import java.io.IOException;
 
 /**
- *
+ * Interface to compress a directory path.
  * @author churas
  */
 public interface CompressDirectory {
     
-    
+    /**
+     * This method should compress the path specified in the <b>inputsDir</b> and
+     * write out a compressed file with the name <b>taskId</b>.(compression)
+     * @param taskId
+     * @param inputsDir
+     * @throws IOException 
+     */
     public void compressDirectory(int taskId,final String inputsDir) throws IOException;
     
 }
