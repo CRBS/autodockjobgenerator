5d9c86f0fa56193d87e7eb4c05988ec7d8c06f52
Christopher Churas <churas@gmail.com>
2013-08-26 13:39:46 -0700

    improved documentation

--- src/main/java/edu/ucsd/crbs/autodockwrapper/io/AutoDockScriptCreator.java
+++ src/main/java/edu/ucsd/crbs/autodockwrapper/io/AutoDockScriptCreator.java
@@ -7,11 +7,19 @@ package edu.ucsd.crbs.autodockwrapper.io;
 import java.io.IOException;
 
 /**
- * PROTOTYPE CODE!!!!!!!!!!!!!!
+ * Interface to create Auto Dock script.
  * @author churas
  */
 public interface AutoDockScriptCreator {
     
+    
+    /**
+     * Implementing methods should create Auto Dock Script under <b>outputJobDir</b> named: {@value edu.ucsd.crbs.autodockwrapper.Constants#AUTO_DOCK_SCRIPT}
+     * @param outputJobDir
+     * @param arguments
+     * @param pathToVina
+     * @throws IOException 
+     */
     public void createAutoDockScript(final String outputJobDir,final String arguments,
                                      final String pathToVina) throws IOException;
 }
