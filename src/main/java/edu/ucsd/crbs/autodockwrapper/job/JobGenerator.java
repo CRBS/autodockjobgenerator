e5ad1c1fe4f1e644492c642c2f8a7906cf09e796
Christopher Churas <churas@gmail.com>
2013-08-26 13:39:33 -0700

    improved documentation

--- src/main/java/edu/ucsd/crbs/autodockwrapper/job/JobGenerator.java
+++ src/main/java/edu/ucsd/crbs/autodockwrapper/job/JobGenerator.java
@@ -7,12 +7,22 @@ package edu.ucsd.crbs.autodockwrapper.job;
 import java.io.IOException;
 
 /**
- * PROTOTYPE CODE!!!!!!!!!!!!!!
+ * Interface that defines methods to create Auto Dock jobs
  * @author churas
  */
 public interface JobGenerator extends java.lang.Runnable {
     
-    public void createJobs() throws IOException;
     
+    /**
+     * This method should create the auto dock jobs
+     * @throws IOException 
+     */
+    public void createJobs() throws IOException;
+
+    /**
+     * This method should return the number of jobs created after {@link #createJobs} is
+     * invoked.
+     * @return Number of jobs created by the {@link #createJobs} method.
+     */
     public long getTotalJobs();
 }
